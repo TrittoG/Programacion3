66ddf38f4d0b21f420051d320baaff6f92777ddc
asd <asd>
2019-05-06 22:05:36 -0300

    apirex

---
+++ Apirest/ejercicio1/clases/alumno.php
@@ -0,0 +1,247 @@
+<?php
+
+    class alumno
+    {
+        public $id;
+        public $nombre;
+        public $contrasena;
+
+
+        
+
+       public function InsertarParametros()
+	    {
+				$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+				$consulta =$objetoAccesoDato->RetornarConsulta("INSERT into usuarios (nombre,contraseña)values(:nombre,:contrasena)");
+				$consulta->bindValue(':nombre',$this->nombre, PDO::PARAM_INT);
+				$consulta->bindValue(':contrasena', $this->contrasena, PDO::PARAM_STR);
+				$consulta->execute();		
+				return $objetoAccesoDato->RetornarUltimoIdInsertado();
+	    }
+       
+        public function TraerUno($request, $response, $args) 
+         {
+            $id=$args['id'];
+              $elCd=alumno::TraerUnCd($id);
+             $newResponse = $response->withJson($elCd, 200);  
+             return $newResponse;
+        }
+
+
+
+        public function TraerTodos($request, $response, $args) 
+        {
+            $todosLosCds=alumno::TraerTodoLosCds();
+            $newResponse = $response->withJson($todosLosCds, 200);  
+        return $newResponse;
+        }
+
+
+
+
+
+
+
+        public function BorrarUno($request, $response, $args) {
+            $ArrayDeParametros = $request->getParsedBody();
+            $id=$ArrayDeParametros['id'];
+            $cd= new alumno();
+            $cd->id=$id;
+            $cantidadDeBorrados=$cd->BorrarCd();
+
+            $objDelaRespuesta= new stdclass();
+            $objDelaRespuesta->cantidad=$cantidadDeBorrados;
+            if($cantidadDeBorrados>0)
+            {
+                    $objDelaRespuesta->resultado="algo borro!!!";
+            }
+            else
+            {
+                $objDelaRespuesta->resultado="no Borro nada!!!";
+            }
+            $newResponse = $response->withJson($objDelaRespuesta, 200);  
+            return $newResponse;
+        }
+
+
+
+        public function ModificarUno($request, $response, $args) {
+                //$response->getBody()->write("<h1>Modificar  uno</h1>");
+                $ArrayDeParametros = $request->getParsedBody();
+                //var_dump($ArrayDeParametros);    	
+                $micd = new alumno();
+                $micd->id=$ArrayDeParametros['id'];
+                $micd->nombre=$ArrayDeParametros['nombre'];
+                $micd->contrasena=$ArrayDeParametros['contrasena'];
+
+                $resultado =$micd->ModificarCdParametros();
+                $objDelaRespuesta= new stdclass();
+                //var_dump($resultado);
+                $objDelaRespuesta->resultado=$resultado;
+                return $response->withJson($objDelaRespuesta, 200);		
+        }
+
+
+
+
+
+
+
+        /* final especiales para slimFramework*/
+        public function BorrarCd()
+        {
+                $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+            $consulta =$objetoAccesoDato->RetornarConsulta("
+                delete 
+                from usuarios 				
+                WHERE id=:id");	
+                $consulta->bindValue(':id',$this->id, PDO::PARAM_INT);		
+                $consulta->execute();
+                return $consulta->rowCount();
+        }
+
+        public static function BorrarCdPorAnio($año)
+        {
+
+            $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+            $consulta =$objetoAccesoDato->RetornarConsulta("
+                delete 
+                from usuarios 				
+                WHERE id=:id");	
+                $consulta->bindValue(':id',$año, PDO::PARAM_INT);		
+                $consulta->execute();
+                return $consulta->rowCount();
+
+        }
+
+        public function ModificarCd()
+        {
+
+            $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+            $consulta =$objetoAccesoDato->RetornarConsulta("
+                update usuarios 
+                set nombre='$this->nombre',
+                contraseña='$this->contrasena',
+                WHERE id='$this->id'");
+            return $consulta->execute();
+
+        }
+   
+ 
+        public function InsertarElCd()
+        {
+                $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+                $consulta =$objetoAccesoDato->RetornarConsulta("INSERT into usuarios (nombre,contraseña)values('$this->nombre','$this->contrasena')");
+                $consulta->execute();
+                return $objetoAccesoDato->RetornarUltimoIdInsertado();
+                
+
+        }
+
+        public function ModificarCdParametros()
+        {
+            $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+            $consulta =$objetoAccesoDato->RetornarConsulta("
+                update usuarios 
+                set nombre=:nombre,
+                contraseña=:contrasena
+                WHERE id=:id");
+            $consulta->bindValue(':id',$this->id, PDO::PARAM_INT);
+            $consulta->bindValue(':nombre',$this->nombre, PDO::PARAM_INT);
+            $consulta->bindValue(':contrasena', $this->contrasena, PDO::PARAM_STR);
+            return $consulta->execute();
+        }
+
+        public function InsertarElCdParametros()
+        {
+                $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+                $consulta =$objetoAccesoDato->RetornarConsulta("INSERT into usuarios (nombre,contraseña)values(:nombre,:contrasena)");
+                $consulta->bindValue(':nombre',$this->nombre, PDO::PARAM_INT);
+                $consulta->bindValue(':contrasena', $this->contrasena, PDO::PARAM_STR);
+                $consulta->execute();		
+                return $objetoAccesoDato->RetornarUltimoIdInsertado();
+        }
+
+        public function GuardarCD()
+        {
+
+            if($this->id>0)
+                {
+                    $this->ModificarCdParametros();
+                }else {
+                    $this->InsertarElCdParametros();
+                }
+
+        }
+
+
+        public static function TraerTodoLosCds()
+        {
+           $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+           $consulta =$objetoAccesoDato->RetornarConsulta("select id,nombre, contraseña from usuarios");
+           $consulta->execute();			
+           return $consulta->fetchAll(PDO::FETCH_CLASS, "alumno");		
+        }
+
+        public static function TraerUnCd($id) 
+        {
+                $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+                $consulta =$objetoAccesoDato->RetornarConsulta("select id, nombre, contraseña from usuarios where id = $id");
+                $consulta->execute();
+                $cdBuscado= $consulta->fetchObject('alumno');
+                return $cdBuscado;				
+
+                
+        }
+
+        public static function TraerUnCdAnio($id,$anio) 
+        {
+                $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+                $consulta =$objetoAccesoDato->RetornarConsulta("select  titel as titulo, interpret as cantante,jahr as año from cds  WHERE id=? AND jahr=?");
+                $consulta->execute(array($id, $anio));
+                $cdBuscado= $consulta->fetchObject('cd');
+                    return $cdBuscado;				
+
+                
+        }
+
+        public static function TraerUnCdAnioParamNombre($id,$anio) 
+        {
+                $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+                $consulta =$objetoAccesoDato->RetornarConsulta("select  titel as titulo, interpret as cantante,jahr as año from cds  WHERE id=:id AND jahr=:anio");
+                $consulta->bindValue(':id', $id, PDO::PARAM_INT);
+                $consulta->bindValue(':anio', $anio, PDO::PARAM_STR);
+                $consulta->execute();
+                $cdBuscado= $consulta->fetchObject('cd');
+                    return $cdBuscado;				
+
+                
+        }
+   
+        public static function TraerUnCdAnioParamNombreArray($id,$anio) 
+        {
+                $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+                $consulta =$objetoAccesoDato->RetornarConsulta("select  titel as titulo, interpret as cantante,jahr as año from cds  WHERE id=:id AND jahr=:anio");
+                $consulta->execute(array(':id'=> $id,':anio'=> $anio));
+                $consulta->execute();
+                $cdBuscado= $consulta->fetchObject('cd');
+                    return $cdBuscado;				
+
+                
+        }
+
+        public function mostrarDatos()
+        {
+                return "Metodo mostar:".$this->titulo."  ".$this->cantante."  ".$this->año;
+        }
+
+
+
+
+        public function login($request, $response, $args)
+        {
+            
+        }
+    }
+
+?>
---
+++ Apirest/ejercicio1/index.php
@@ -0,0 +1,171 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require_once '../composer/vendor/autoload.php';
+require_once '/clases/AccesoDatos.php';
+require_once '/clases/alumno.php';
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+/*
+
+¡La primera línea es la más importante! A su vez en el modo de 
+desarrollo para obtener información sobre los errores
+ (sin él, Slim por lo menos registrar los errores por lo que si está utilizando
+  el construido en PHP webserver, entonces usted verá en la salida de la consola 
+  que es útil).
+
+  La segunda línea permite al servidor web establecer el encabezado Content-Length, 
+  lo que hace que Slim se comporte de manera más predecible.
+*/
+
+$app = new \Slim\App(["settings" => $config]);
+
+//require_once "saludo.php";
+
+
+$app->get('[/]', function (Request $request, Response $response) {    
+    $response->getBody()->write("GET => Bienvenido!!! ,a SlimFramework");
+    return $response;
+
+});
+
+$app->post('[/]', function (Request $request, Response $response) {   
+    $response->getBody()->write("POST => Bienvenido!!! ,a SlimFramework");
+    return $response;
+
+});
+
+$app->put('[/]', function (Request $request, Response $response) {  
+    $response->getBody()->write("PUT => Bienvenido!!! ,a SlimFramework");
+    return $response;
+
+});
+
+$app->delete('[/]', function (Request $request, Response $response) {  
+    $response->getBody()->write(" DELETE => Bienvenido!!! ,a SlimFramework");
+    return $response;
+
+});
+
+
+
+$app->get('/datos/', function (Request $request, Response $response) {     
+    $datos = array('nombre' => 'rogelio','apellido' => 'agua', 'edad' => 40);
+    $newResponse = $response->withJson($datos, 200);  
+    return $newResponse;
+});
+
+$app->post('/datos/', function (Request $request, Response $response) {    
+    $ArrayDeParametros = $request->getParsedBody();
+   // var_dump($ArrayDeParametros);
+    $objeto= new stdclass();
+    $objeto->nombre=$ArrayDeParametros['nombre'];
+    $objeto->apellido=$ArrayDeParametros['contraseña'];
+    $newResponse = $response->withJson($objeto, 200);  
+    return $newResponse;
+
+});
+
+/* atender todos los verbos de HTTP*/
+$app->any('/cualquiera/[{id}]', function ($request, $response, $args) {
+    
+    var_dump($request->getMethod());
+    $id=$args['id'];
+    $response->getBody()->write("cualquier verbo de ajax parametro: $id ");
+    return $response;
+});
+
+
+
+/* atender algunos los verbos de HTTP*/
+$app->map(['GET', 'POST'], '/mapeado', function ($request, $response, $args) {
+
+      var_dump($request->getMethod());
+     $response->getBody()->write("Solo POST y GET");
+});
+
+
+/* agrupacion de ruta*/
+$app->group('/saludo', function () {
+
+    $this->get('/{nombre}', function ($request, $response, $args) {
+        $nombre=$args['nombre'];
+        $response->getBody()->write("HOLA, Bienvenido <h1>$nombre</h1> a la apirest de 'CDs'");
+    });
+
+     $this->get('/', function ($request, $response, $args) {
+        $response->getBody()->write("HOLA, Bienvenido a la apirest de 'CDs'... ingresá tu nombre");
+    });
+ 
+     $this->post('/', function ($request, $response, $args) {      
+        $response->getBody()->write("HOLA, Bienvenido a la apirest por post");
+    });
+     
+});
+
+
+/* agrupacion de ruta y mapeado*/
+$app->group('/usuario/{id:[0-9]+}', function () {
+
+    $this->map(['POST', 'DELETE'], '', function ($request, $response, $args) {
+        $response->getBody()->write("Borro el usuario por p");
+    });
+
+    $this->get('/nombre', function ($request, $response, $args) {
+        $response->getBody()->write("Retorno el nombre del usuario del id ");
+    });
+});
+
+
+
+
+/*LLAMADA A METODOS DE INSTANCIA DE UNA CLASE*/
+$app->group('/alumno', function () {   
+
+$this->get('/', \alumno::class . ':traerTodos');
+$this->get('/{id}', \alumno::class . ':traerUno');
+$this->delete('/', \alumno::class . ':BorrarUno');
+$this->put('/', \alumno::class . ':ModificarUno');
+//$this->post('[/]', \alumno::class . ':CargarUno');
+//se puede tener funciones definidas
+/*SUBIDA DE ARCHIVO*/
+
+$this->post('/', function (Request $request, Response $response) {
+  
+    
+    $ArrayDeParametros = $request->getParsedBody();
+    //var_dump($ArrayDeParametros);
+    $nombre= $ArrayDeParametros['nombre'];
+    //var_dump($ArrayDeParametros);
+    $contrasena= $ArrayDeParametros['contrasena'];
+   
+    $miAlumno = new alumno();
+    $miAlumno->nombre=$nombre;
+    $miAlumno->contrasena=$contrasena;
+    var_dump($miAlumno);
+    $miAlumno->InsertarParametros();
+
+    return $response;
+
+});
+
+     
+});
+
+
+
+$app->group('/login', function () {   
+
+    $this->post('/', \alumno::class . ':login');
+    
+    
+    
+      
+});
+
+
+
+$app->run();
